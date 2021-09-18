c3996162ffc0315ad95658158b0bae37eaa3dce6
dimitramuni <muni.dimitra@gmail.com>
2021-09-18 11:24:16 +0200

    ad astra

--- script/Identify.R
+++ script/Identify.R
@@ -1,10 +1,11 @@
 ## Identifyinig regime change in the data set
 ## Based on work the by Bendtsen M. (2017), https://link.springer.com/article/10.1007/s10618-017-0510-5
-
-
+setwd("~/Desktop/GBN-Regime-in-Basketball/script")
+source('uniform_distribution.R')
+source('beta_proposal.R')
 require(textshape)
 require(bnlearn)
-
+require(Rlab)
 dataset=asia
 n=dim(dataset)[1]
 
@@ -63,7 +64,7 @@ repeat{                  #L9
     if(dim(D_current[[i]])[1]>0.30){
       #structure learning for subset i of D_proposal dataset
       bn<-hc(D_current[[i]],score = 'bde')
-      #bayesian dirichlet score 
+      #bayesian dirichlet score #23
       loglikelihood_D_current_nonzero_deltas=
         loglikelihood_D_current_nonzero_deltas+ score(bn, D_current[[i]], type = "bde")
     }
@@ -85,17 +86,28 @@ repeat{                  #L9
       
       #structure learning for subset i of D_proposal dataset
       bn<-hc(D_proposal[[i]],score = 'bde')
-      #bayesian dirichlet score 
+      #bayesian dirichlet score #L25
       loglikelihood_D_proposal_nonzero_deltas=
         loglikelihood_D_proposal_nonzero_deltas+ score(bn, D_proposal[[i]], type = "bde")
     }
     
   }
   
+  #Prior distribution for current beta varaibles
+  U_distribution_current=uniform_distribution(betas = beta_current,n=n,k=k)
+  #Prior distribution for proposal Indicator variables 
+  I_distribution_current= dbern(x=beta_current,prob=0.5,log = TRUE)
   
+  #Prior distribution for proposal beta varaibles
+  U_distribution_proposal=uniform_distribution(betas = beta_proposal,n=n,k=k)
+  #Prior distribution for proposal Indicator variables 
+  I_distribution_proposal=dbern(x=beta_proposal,prob=0.5,log = TRUE)
   
+  #adding logarithmic probability current distributions #L24
+  log_posterior_D_current=loglikelihood_D_current_nonzero_deltas+U_distribution_current+I_distribution_current
   
-  
+  #adding logarithmic probability proposal distributions #L26
+  log_posterior_D_proposal=loglikelihood_D_proposal_nonzero_deltas+ U_distribution_proposal+I_distribution_proposal
   
   }
 
