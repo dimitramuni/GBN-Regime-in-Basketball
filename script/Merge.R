b0057176e12702a71c9baf2528121086c1904dc2
dimitramuni <muni.dimitra@gmail.com>
2021-09-20 11:48:08 +0200

    ad astra

---
+++ script/Merge.R
@@ -0,0 +1,3 @@
+## Mergining Regimes identified by nonzero delts in the data set
+## Based on work the by Bendtsen M. (2017), https://link.springer.com/article/10.1007/s10618-017-0510-5
+## See Appendix A.2
