54b8f1e2ee28a1506bfe6566ec3bfc0b858e36e2
dimitramuni <muni.dimitra@gmail.com>
2021-11-21 16:45:32 +0100

    experiments with abn package

--- script/BN_Structure_Learning_ChicagoBulls.R
+++ script/BN_Structure_Learning_ChicagoBulls.R
@@ -29,37 +29,41 @@ colnames(basic)<-c('Date','Sep','Opp','WL','TmScore','OppScore',
 advanced=read.csv('Chicago_advanced_gamelog_8081_2021.csv')
 colnames(advanced)<-c('Date','Sep','Opp','WL','TmScore','OppScore',
                       'ORtg','DRtg','Pace','FTr','3PAr','TSper','TRBper','ASTper','STLper','BLKper',
-                      'OffeFGper','OffTOVper','OffDRBper','OffFT/FGA',
+                      'OffeFGper','OffTOVper','OffORBper','OffFT/FGA',
                       'DefeFGper','DefTOVper','DefDRBper','DefFT/FGA')
 chicago_gamelog=merge(basic,advanced,by = c('Date','Sep','Opp','WL','TmScore','OppScore'))
 
 #Experiment I
 
 
-'Modeling BN using four factors (offensive and defensive),Game Outcome, Opponent'
+#Modeling BN using 25 variables from basic and advance gamelog stat
 
 ## Pre-processing 
 
-dataset1=subset(chicago_gamelog,select=c('Date','Opp','WL',
+dataset1=subset(chicago_gamelog,select=c('Date','Sep','Opp','WL',
                                          'TmFGper','Tm3Pper','TmFTper',
                                          'OppFGper','Opp3Pper','OppFTper',
                                          'FTr','TSper','TRBper','ASTper','STLper','BLKper',
-                                         'OffeFGper','OffTOVper','OffDRBper','OffFT/FGA',
+                                         'OffeFGper','OffTOVper','OffORBper','OffFT/FGA',
                                         'DefeFGper','DefTOVper','DefDRBper','DefFT/FGA'))
 
 #omiting the rows with missing values,https://statisticsglobe.com/r-remove-data-frame-rows-with-some-or-all-na
 
 gamelog_stat_chicago<-dataset1 %>% drop_na()
+
+
+#deciding if the game was played at home or away
+gamelog_stat_chicago$HomeAway<- ifelse(gamelog_stat_chicago$Sep=='@','Away','Home')
+gamelog_stat_chicago <-gamelog_stat_chicago[,-c(2)] 
+
 #converting percentages to a number between 0 and 1
-
-
 gamelog_stat_chicago[,c('TRBper','ASTper','STLper',
                         'BLKper','OffTOVper',
                         'OffDRBper','DefTOVper',
                         'DefDRBper')]<-gamelog_stat_chicago[,c('TRBper',
                                                                'ASTper','STLper',
                                                                'BLKper','OffTOVper',
-                                                               'OffDRBper','DefTOVper',
+                                                               'OffORBper','DefTOVper',
                                                                'DefDRBper')]/100
 
 
@@ -89,28 +93,30 @@ for (i in 1:n) {
   gamelog_stat_chicago$PlayOff[i]= grepl(gamelog_stat_chicago$Opp[i], playoff_appearance$Teams[playoff_row_index])
   
 }
+write.csv(gamelog_stat_chicago,'PreProcessed_Chicago_Gamelog.csv',row.names = FALSE)
 
 
 
 
-
-gamelog_discrete_chicago<-discretize(gamelog_stat_chicago[,-c(1,2,3,24)], breaks = 10)
+gamelog_discrete_chicago<-discretize(gamelog_stat_chicago[,-c(1,2,3,24,25)], breaks = 5)
 #gamelog_discrete_chicago$Opp<-factor(gamelog_stat_chicago$Opp)
 gamelog_discrete_chicago$WL<-factor(gamelog_stat_chicago$WL)
 gamelog_discrete_chicago$OppPlayoff<-factor(gamelog_stat_chicago$PlayOff)
-
-
+gamelog_discrete_chicago$HomeAway<-factor(gamelog_stat_chicago$HomeAway)
 
 
 setwd("~/Desktop/GBN-Regime-in-Basketball/script")
-bn1<-hc(gamelog_discrete_chicago,score = 'bde')
+bn1<-hc(gamelog_discrete_chicago[1:1228,],score = 'bde')
 bnlearn::score(bn1,gamelog_discrete_chicago)
 graphviz.plot(bn1)
 
-bn2<-tabu(gamelog_discrete_chicago,score='bde')
+bn2<-hc(gamelog_discrete_chicago[1229:2549,],score='bde')
 bnlearn::score(bn2,gamelog_discrete_chicago)
 graphviz.plot(bn2)
 
+bn3<-hc(gamelog_discrete_chicago[2550:dim(gamelog_discrete_chicago)[1],],score='bde')
+bnlearn::score(bn3,gamelog_discrete_chicago)
+graphviz.plot(bn3)
 
 #bn3<-rsmax2(gamelog_discrete_chicago)
 #bnlearn::score(bn3,gamelog_discrete_chicago)
